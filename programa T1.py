c34eb5e2de614e3ce031352dfbfd687e9caad586
Antonio colli <36493960+antonioCC@users.noreply.github.com>
2021-03-26 22:33:02 -0600

    Update programa T1.py

--- programa T1.py
+++ programa T1.py
@@ -1,56 +1,49 @@
 import re
 #patron = re.compile('a[3-5]+')# coincide con una letra, seguida de al menos 1 dígito entre 3 y 5
 #print( input("Introduce un texto") );
-archivo=open('datosA.txt','r')
-#print("el texto es: "+archivo.read())
+import re
+
+archivo=open("datosA.txt","r")
 mensaje=archivo.read()
 
 
-print("ingresa un una opcion")
-print("1: variable entera")
-print("2: variable real")
-print("3: variable de cadena")
-print("4: variable logica")
-numero = int(input())
 
-if numero==1:
-	#variable entera
-	patron = re.compile('((int |long |byte |short )[A-Za-z]+\w?\=\-?(\d{1,7})((\+|\-|\*|\/)\d{1,7})?\;)')
-	print (patron.findall(mensaje))
-	if patron.findall(mensaje)!=[]:
-		print("asignacion entera exitosa")
+p1 = re.compile('(int |long |byte |short )')
+p2 = re.compile('(double |float )')
+p3 = re.compile('(String )')
+p4 = re.compile('(boolean )')
+
+
+
+if p1.findall(mensaje)!=[]:#condicion para encontrar el primer patron (int, long,byte,short)
+	patron = re.compile('((int |long |byte |short )([A-Za-z]+\w?\=\-?(\d{1,7})((\+|\-|\*|\/)\d{1,7})?\;))')# expresion regular
+	if patron.findall(mensaje)!=[]:#condicion para encontrar la expresion regular
+		print("asignacion entera exitosa: "+str(patron.findall(mensaje)))#imprime exitoso en caso de ser correcto
 	else:
-		print("error en la asignacion de una variable entera")
+		print("error en la asignacion de una variable entera")#mensaje en caso de ser erroneo
 	archivo.close()
-
-
-if numero==2:
-	#variable real
-	patron = re.compile('((double |float )([A-Za-z]+\w?\=\-?\d{1,7}\.\d{1,7}f?((\+|\-|\*|\/)\d{1,7}\.\d{1,7}f?)?\;))')
-	print (patron.findall(mensaje))
-	if patron.findall(mensaje)!=[]:
-		print("asignacion real exitosa")
+elif p2.findall(mensaje)!=[]:#condicion para encontrar el primer patron (double,float)
+#variable real
+	patron = re.compile('((double |float )([A-Za-z]+\w?\=\-?\d{1,7}\.\d{1,7}f?((\+|\-|\*|\/)\d{1,7}\.\d{1,7}f?)?\;))')# expresion regular
+	if patron.findall(mensaje)!=[]:#condicion para encontrar la expresion regular
+		print("asignacion real exitosa: "+str(patron.findall(mensaje)))#imprime exitoso en caso de ser correcto
 	else:
-		print("error en la asignacion de una variable real")
+		print("error en la asignacion de una variable real")#mensaje en caso de ser erroneo
 	archivo.close()
-
-if numero==3:
-	#variable de cadena
-	patron = re.compile('(String [A-Za-z]+\w?\=\"(\w(.+)?)?\"(\+\"(\w(.+)?)?\")?\;)')
-	print (patron.findall(mensaje))
-	if patron.findall(mensaje)!=[]:
-		print("asignacion de cadena exitosa")
+elif p3.findall(mensaje)!=[]:#condicion para encontrar el primer patron (String)
+#variable de cadena
+	patron = re.compile('(String [A-Za-z]+\w?\=\"(\w(.+)?)?\"(\+\"(\w(.+)?)?\")?\;)')# expresion regular
+	if patron.findall(mensaje)!=[]:#condicion para encontrar la expresion regular
+		print("asignacion de cadena exitosa: "+str(patron.findall(mensaje)))#imprime exitoso en caso de ser correcto
 	else:
-		print("error en la asignacion de una variable de cadena")
+		print("error en la asignacion de una variable de cadena")#mensaje en caso de ser erroneo
 	archivo.close()
-
-if numero==4:
-	#variable logica 
-	print( "VALIDACION DE DATOS LOGICOS 10 + 5 = 10")
-	logico = bool(10 + 5 == 15)
-	print (logico)
-
-	print( "Erros de asignacion de datos logicos")
-	logico = bool(2 >" hola ")
-	print (logico)
+#variable de boolean
+elif p4.findall(mensaje)!=[]:#condicion para encontrar el primer patron (boolean)
+	patron = re.compile('(boolean [A-Za-z]+\w?\=(\-?\d{1,7}(\.\d{1,7})?(\<|\>|\=\=|\<\=|\>\=)\d{1,7}(\.\d{1,7})?|true|false)\;)')# expresion regular
+	if patron.findall(mensaje)!=[]:#condicion para encontrar la expresion regular
+		print("asignacion logica exitosa: "+str(patron.findall(mensaje)))#imprime exitoso en caso de ser correcto 
+	else:
+		print("error en la asignacion de una variable logica")#mensaje en caso de ser erroneo
+	archivo.close()
 
